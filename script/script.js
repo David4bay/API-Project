d3d3a097920aac4b695685202749ea7c3cee25b6
David4bay <david4bay@gmail.com>
2023-03-17 02:35:34 +0100

    fixed bugs

--- script/script.js
+++ script/script.js
@@ -5,21 +5,42 @@ const form = document.getElementById('joke-form');
 const jokes = document.getElementsByClassName('joke');
 const punchLine = document.getElementsByClassName('punchline');
 const errorMessage = document.getElementsByClassName('error');
-
-let numberOfJokes = +select.value;
+let numberOfJokes = Number(select.options[select.selectedIndex].value);
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
+    let numberOfJokes = Number(select.options[select.selectedIndex].value);
+    console.log(numberOfJokes)
     highlightLabel();
     fetch('https://official-joke-api.appspot.com/jokes/ten').then((response) => response.json()).then((data) => showJoke(data)).catch(() => showError())
-})
 
-form.addEventListener('change', (e) => {
-    e.preventDefault();
-})
-
-select.addEventListener('change', (e) => {
-    e.preventDefault();
+    function showJoke(data) {
+        for (let i = 0; i < numberOfJokes; i++) {
+            jokes[i].style.display = 'block';
+            jokes[i].innerText = data[i].setup;
+            setTimeout(() => {
+                punchLine[i].style.display = 'block';
+                punchLine[i].innerText = data[i].punchline;
+            }, 3000);
+            setTimeout(() => {
+                jokes[i].innerText = '';
+                jokes[i].style.display = 'none';
+                punchLine[i].innerText = '';
+                punchLine[i].style.display = 'none';
+            }, 8000);
+        }
+        console.log(numberOfJokes);
+    }
+    
+    function showError() {
+        for (let i = 0; i < numberOfJokes; i++) {
+            errorMessage[i].innerText = 'Sorry, request for joke failed!';
+            setTimeout(() => {
+                errorMessage[i].innerText = '';
+            }, 10000)
+        }
+        console.log('sorry there was an error somewhere');
+    }
 })
 
 function highlightLabel() {
@@ -30,29 +51,3 @@ function highlightLabel() {
     }, 1000);
 }
 
-function showJoke(data) {
-    for (let i = 0; i < numberOfJokes; i++) {
-        jokes[i].style.display = 'block';
-        jokes[i].innerText = data[i].setup;
-        setTimeout(() => {
-            punchLine[i].style.display = 'block';
-            punchLine[i].innerText = data[i].punchline;
-        }, 3000);
-        setTimeout(() => {
-            jokes[i].innerText = '';
-            jokes[i].style.display = 'none';
-            punchLine[i].innerText = '';
-            punchLine[i].style.display = 'none';
-        }, 30000);
-    }
-}
-
-function showError() {
-    for (let i = 0; i < numberOfJokes; i++) {
-        errorMessage[i].innerText = 'Sorry, request for joke failed!';
-        setTimeout(() => {
-            errorMessage[i].innerText = '';
-        }, 10000)
-    }
-    console.log('sorry there was an error somewhere');
-}
